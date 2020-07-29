071d083503ab9326c4810fe66236c270ff83b521
Anders Iglebekk <andersiglebekk@AIMBPR15.local>
2020-07-29 09:51:10 +0200

    updated view

--- aksjeliste.py
+++ aksjeliste.py
@@ -4,8 +4,14 @@ from tabulate import tabulate
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
+def clean_up(var):
+    location = var.find("%")+1
+    var = var[1:location]
+    return(var)
 
-def get_data(page):
+
+
+def get_data(page, sign):
     soup = BeautifulSoup(page.text, 'html.parser')
     s = soup.find('a', class_='c02393 c02394').find_all(text=True, recursive=False)
     names = soup.find_all('td', class_='c02380 c02383 c02387 c02381 borderBottom c02386 c02382')
@@ -16,7 +22,8 @@ def get_data(page):
     rows = list()
     for name in names:          # Print all occurrences
         if count < max_c:
-            rows.append([str(name.get_text()), str(values[count].get_text())])
+            value = sign + clean_up(values[count].get_text())
+            rows.append([str(name.get_text()), clean_up(str(value))])
             count = count + 1
 
     print(
@@ -32,10 +39,10 @@ if __name__ == "__main__":
     print('')
     print("******************************VINNERE******************************")
     page = requests.get("https://www.nordnet.no/market/stocks?selectedTab=prices&sortField=diff_pct&sortOrder=desc&exchangeCountry=NO")
-    get_data(page)
+    get_data(page, "+")
     print('')
     print("******************************TAPERE******************************")
     page = requests.get("https://www.nordnet.no/market/stocks?selectedTab=prices&sortField=diff_pct&sortOrder=asc&exchangeCountry=NO")
-    get_data(page)
+    get_data(page, "-")
     print('')
     print('')
